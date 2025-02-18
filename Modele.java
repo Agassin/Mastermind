54dc5b4b97e0200ab81ecde07de3684dcfcf55e1
Agassin <129422340+Agassin@users.noreply.github.com>
2025-02-18 15:03:07 +0100

    Create Modele.java

---
+++ Modele.java
@@ -0,0 +1,9 @@
+
+import java.awt.Color;
+
+class Modele {
+    Color[] couleurs = {Color.yellow, Color.BLACK, Color.GREEN, Color.magenta,Color.red, Color.orange,Color.white};
+    int N_TENTATIVES = 10;
+    int DIFFICULTE = 4;
+  
+}
