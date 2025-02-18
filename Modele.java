f50662a8c3faba4f2f870c4b6aeaa05d98d125e8
MistiXe <37303853+MistiXe@users.noreply.github.com>
2025-02-18 17:31:18 +0100

    Update Modele.java

--- Modele.java
+++ Modele.java
@@ -1,10 +1,12 @@
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 class Modele {
     Color[] couleurs = {Color.yellow, Color.BLUE,Color.BLACK, Color.GREEN, Color.magenta,Color.red, Color.orange,Color.white};
     int N_TENTATIVES = 10;
     static int DIFFICULTE = 4;
+    ArrayList<Color> ar_c = new ArrayList<>();
     enum Etat {
         EN_COURS,
         GAGNE,
@@ -12,6 +14,8 @@ class Modele {
     }
     public void completerRangee(int i){
         System.out.println("ajout de la couleur :"+couleurs[i]);
+        ar_c.add(couleurs[i]);
+        System.out.println(ar_c);
     }
 }
 
