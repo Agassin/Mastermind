b34c1573db9722eeb3c9bfca2439bae9a81a9526
Agassin <129422340+Agassin@users.noreply.github.com>
2025-02-18 15:34:05 +0100

    finition de Modèle +rangée

--- Modele.java
+++ Modele.java
@@ -4,6 +4,11 @@ import java.awt.Color;
 class Modele {
     Color[] couleurs = {Color.yellow, Color.BLACK, Color.GREEN, Color.magenta,Color.red, Color.orange,Color.white};
     int N_TENTATIVES = 10;
-    int DIFFICULTE = 4;
-  
+    static int DIFFICULTE = 4;
+    enum Etat {
+        EN_COURS,
+        GAGNE,
+        PERDU
+    }
+    
 }
